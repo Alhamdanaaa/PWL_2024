198e285fda175ef4688cc8ca32fad649a0185d31
Alhamdanaaa <aalhamdana@gmail.com>
2024-02-21 11:48:39 +0700

    jobsheet 2 prak 3 perubahn view controller

--- routes/web.php
+++ routes/web.php
@@ -63,4 +63,8 @@ Route::resource('photos', PhotoController::class)->only([ 'index', 'show'
 Route::resource('photos', PhotoController::class)->except([ 'create', 'store', 'update', 'destroy'
 ]);
 
-// 
+// Route::get('/greeting', function(){
+//     return view('blog.hello', ['name' => 'Alhamdana']);
+// });
+
+Route::get('/greeting', [WelcomeController::class,'greeting']);
