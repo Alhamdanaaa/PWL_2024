8fef668d1abd99ec0f9031f711e2929131a4d345
Alhamdanaaa <aalhamdana@gmail.com>
2024-02-24 17:45:55 +0700

    route list

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/user/{name?}', function ($name='John') {
 });
 
 
-Route::resource('photos', PhotoController::class);
+// Route::resource('photos', PhotoController::class);
 
 Route::resource('photos', PhotoController::class)->only([ 'index', 'show'
 ]);
